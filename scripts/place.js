90f520fafa9574321256543e889dd4b452ccdc4a
flepdahflep <jfernandez4@byupathway.edu>
2026-07-17 23:48:33 +0800

    W3 Assignment

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,23 @@ const modified = document.querySelector("#lastModified");
 year.textContent = new Date().getFullYear();
 
 modified.textContent = document.lastModified;
+
+const temperature = 28;
+const windSpeed = 6;
+
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
+    ).toFixed(1);
+}
+
+const windChill = document.querySelector("#windChill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
+} else {
+    windChill.textContent = "N/A";
+}
