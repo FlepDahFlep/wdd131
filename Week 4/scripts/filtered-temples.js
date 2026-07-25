5fcc968494d61fbd80278428d5628e6d12660dca
flepdahflep <jfernandez4@byupathway.edu>
2026-07-25 12:40:45 +0800

    W4 Assignment

--- Week 4/scripts/filtered-temples.js
+++ Week 4/scripts/filtered-temples.js
@@ -12,70 +12,63 @@ hamButton.addEventListener("click", () => {
     hamButton.classList.toggle("open");
 });
 
+
 const temples = [
   {
     templeName: "Nauvoo Temple",
     location: "Nauvoo, Illinois",
     dedicated: "1846, May, 1",
     area: 50000,
-    imageUrl:
-    "https://s1.ezgif.com/tmp/ezgif-1e0bbfb897fed4f2.webp"
+    image: "images/nauvoo-temple.webp"
   },
   {
     templeName: "Salt Lake Temple",
     location: "Salt Lake City, Utah",
     dedicated: "1893, April, 6",
     area: 382207,
-    imageUrl:
-    "https://s7.ezgif.com/tmp/ezgif-7e44d542560c9016.webp"
+    image: "images/salt-lake-temple.webp"
   },
   {
     templeName: "Manila Temple",
     location: "Quezon City, Manila",
     dedicated: "1984, September, 25",
     area: 26683,
-    imageUrl:
-    "https://s7.ezgif.com/tmp/ezgif-7c3be09287bcf09e.webp"
+    image: "images/manila-temple.webp"
   },
   {
     templeName: "Cebu Temple",
     location: "Brgy. Lahug, Cebu City, Cebu",
     dedicated: "2010, June, 13",
     area: 29556,
-    imageUrl:
-    "https://s7.ezgif.com/tmp/ezgif-74bbf05fc08a7db0.webp"
+    image: "images/cebu-temple.webp"
   },
   {
     templeName: "Urdaneta Temple",
     location: "Brgy. Nancayasan, Urdaneta City, Pangasinan",
     dedicated: "2024, April, 28",
     area: 32604,
-    imageUrl:
-    "https://s7.ezgif.com/tmp/ezgif-786f6a6c8f1e3bac.webp"
+    image: "images/urdaneta-temple.webp"
   },
   {
     templeName: "Alabang Temple",
     location: "Muntinlupa City, Metro Manila",
     dedicated: "2026, January, 18",
     area: 6861,
-    imageUrl:
-    "https://s7.ezgif.com/tmp/ezgif-7c59dd7aedf15233.webp"
+    image: "images/alabang-temple.webp"
   },
   {
     templeName: "Davao Temple",
     location: "Davao City, Davao Del Sur",
     dedicated: "2026, May, 3",
     area: 18450,
-    imageUrl:
-    "https://s7.ezgif.com/tmp/ezgif-781af2be53b5cd3c.webp"
+    image: "images/davao-temple.webp"
   },
   {
     templeName: "Bacolod Temple",
     location: "Bacolod, Philippines",
     dedicated: "2026, May, 31",
     area: 9600,
-    imageUrl:
-    "https://s7.ezgif.com/tmp/ezgif-7590feace0eb5c84.webp"
+    image: "images/bacolod-temple.webp"
   },
 ];
 
@@ -106,7 +99,7 @@ function displayTemples(temples) {
         area.innerHTML =
             `<strong>Area:</strong> ${temple.area.toLocaleString()} sq ft`;
 
-        image.src = temple.imageUrl;
+        image.src = temple.image;
         image.alt = temple.templeName;
         image.loading = "lazy";
         image.width = 400;
